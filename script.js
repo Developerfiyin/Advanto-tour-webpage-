67d7bed5f1a9e9b8f00753f1d550694298d41770
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-05-24 21:39:36 +0100

    updated script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const menuBtn = document.getElementById("menu-btn");
+const navoLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navoLinks.classList.toggle("open");
+
+  const isOpen = navoLinks.classList.contains("open");
+
+  menuBtnIcon.setAttribute(
+    "class",
+    isOpen ? "ri-close-line" : "ri-menu-3-line"
+  );
+});
